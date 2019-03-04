747aafd5092bc09a442b9086114b4b2715ae0932
Peter Tsongalis <hhgoal14@gmail.com>
2019-03-03 20:54:22 -0500

    under construction commits

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Peter Tsongalis - Portfolio under construction.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
